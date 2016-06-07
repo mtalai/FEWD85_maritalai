6a5f8f91499576e0ded4dc28fd41caed9f79de8e
mtalai <mari.talai@viacom.com>
2016-06-07 18:01:31 -0400

    adding js and such

---
+++ FEWD_85_Final Project-Mari_Talai/js/index.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+	if ($(".hamburger").css('display') == 'block') {
+		$( ".cross" ).hide();
+		$( ".menu-item" ).hide();
+		$( ".hamburger" ).click(function() {
+			$( ".menu-item" ).slideToggle( "slow", function() {
+				$( ".hamburger" ).hide();
+				$( ".cross" ).show();
+			});
+		});
+
+		$( ".cross" ).click(function() {
+			$( ".menu-item" ).slideToggle( "slow", function() {
+				$( ".cross" ).hide();
+				$( ".hamburger" ).show();
+			});
+		});
+	}
+
+
+	$('.heading-text-animation').textillate({
+		selector: '.texts',
+		loop: true,
+		minDisplayTime: 5000,
+		initialDelay: 0,
+		autoStart: true,
+		inEffects: [ 'fadeInLeft'],
+		outEffects: ['fadeOutRight'],
+		in: {
+			effect: 'fadeInLeft',
+			delayScale: 1.5,
+			delay: 50,
+			sync: false,
+			shuffle: false,
+			reverse: false,
+			callback: function () {}
+		},
+		out: {
+			effect: 'fadeOutRight',
+			delayScale: 1.5,
+			delay: 50,
+			sync: false,
+			shuffle: false,
+			reverse: false,
+			callback: function () {}
+		},
+		callback: function () {},
+		type: 'char'
+	}); 
+
+});
+
+
